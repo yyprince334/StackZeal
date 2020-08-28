0692f5250ade821ba3e87a93f6a6c3cc45829d97
jamsher <jamshersekh2345@gmail.com>
2020-08-28 20:14:00 +0530

    This is first commit

---
+++ app/src/main/java/com/james/stack_zeal/second_activity.java
@@ -0,0 +1,14 @@
+package com.james.stack_zeal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class second_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_activity);
+    }
+}
